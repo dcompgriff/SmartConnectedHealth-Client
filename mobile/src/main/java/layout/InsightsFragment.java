ddf9b5d4b103fbec1f1c3ae180967b6de3c51ca6
Dan Griffin <unknown7979@gmail.com>
2016-03-21 16:33:38 -0400

    Added graph plotting code, and an example radar chart plot. 1) Added gradle dependencies to import MPAndroidChart. 2) Added code to Insights fragment to plot example spider plot data. 3) Added attributes and marker view xml frament for drawing a numeric marker on top of a spyder plot. 4) Added MyMarkerView, which draws a numeric marker on top of the spider plot.

--- mobile/src/main/java/layout/InsightsFragment.java
+++ mobile/src/main/java/layout/InsightsFragment.java
@@ -1,14 +1,28 @@
 package layout;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.Legend.LegendPosition;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.sch.trustworthysystems.smartconnectedhealth_client.R;
+import com.sch.trustworthysystems.smartconnectedhealth_client.view.MyMarkerView;
+
+import java.util.ArrayList;
+
+//Try to import one of the plotting libraries.
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +40,10 @@ public class InsightsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // Chart variables
+    private RadarChart mChart;
+    //private Typeface tf;
 
     public InsightsFragment() {
         // Required empty public constructor
@@ -66,9 +84,100 @@ public class InsightsFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        //Retrieve a reference to the radar chart.
+        mChart = (RadarChart) getActivity().findViewById(R.id.meal_radar_chart);
+
+        //tf = Typeface.createFromAsset(getActivity().getAssets(), "OpenSans-Regular.ttf");
+
+        MyMarkerView mv = new MyMarkerView(getActivity(), R.layout.custom_marker_view);
+        // set the marker to the chart
+        mChart.setMarkerView(mv);
+
+        mChart.invalidate();
+        // Setup chart visuals
+        mChart.setDescription("Post Meals");
+        mChart.setWebLineWidth(1.5f);
+        mChart.setWebLineWidthInner(0.75f);
+        mChart.setWebAlpha(100);
+
+        setData();
+
+        mChart.animateXY(
+                1400, 1400,
+                Easing.EasingOption.EaseInOutQuad,
+                Easing.EasingOption.EaseInOutQuad);
+
+        XAxis xAxis = mChart.getXAxis();
+        //xAxis.setTypeface(tf);
+        xAxis.setTextSize(9f);
+
+        YAxis yAxis = mChart.getYAxis();
+        //yAxis.setTypeface(tf);
+        yAxis.setLabelCount(5, false);
+        yAxis.setTextSize(9f);
+        yAxis.setAxisMinValue(0f);
+
+        Legend l = mChart.getLegend();
+        l.setPosition(LegendPosition.RIGHT_OF_CHART);
+        //l.setTypeface(tf);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(5f);
+    }
 
 
 
+    private String[] mParties = new String[]{
+            "Party A", "Party B", "Party C", "Party D", "Party E", "Party F", "Party G", "Party H",
+            "Party I"
+    };
+    public void setData() {
+
+        float mult = 150;
+        int cnt = 9;
+
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+        ArrayList<Entry> yVals2 = new ArrayList<Entry>();
+
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        for (int i = 0; i < cnt; i++) {
+            yVals1.add(new Entry((float) (Math.random() * mult) + mult / 2, i));
+        }
+
+        for (int i = 0; i < cnt; i++) {
+            yVals2.add(new Entry((float) (Math.random() * mult) + mult / 2, i));
+        }
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < cnt; i++)
+            xVals.add(mParties[i % mParties.length]);
+
+        RadarDataSet set1 = new RadarDataSet(yVals1, "Set 1");
+        set1.setColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        set1.setFillColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        set1.setDrawFilled(true);
+        set1.setLineWidth(2f);
+
+        RadarDataSet set2 = new RadarDataSet(yVals2, "Set 2");
+        set2.setColor(ColorTemplate.VORDIPLOM_COLORS[4]);
+        set2.setFillColor(ColorTemplate.VORDIPLOM_COLORS[4]);
+        set2.setDrawFilled(true);
+        set2.setLineWidth(2f);
+
+        ArrayList<IRadarDataSet> sets = new ArrayList<IRadarDataSet>();
+        sets.add(set1);
+        sets.add(set2);
+
+        RadarData data = new RadarData(xVals, sets);
+        //data.setValueTypeface(tf);
+        data.setValueTextSize(8f);
+        data.setDrawValues(false);
+
+        mChart.setData(data);
+
+        mChart.invalidate();
     }
 
     @Override
